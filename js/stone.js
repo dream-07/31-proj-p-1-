44fe405cb2c759054543a6bb3b775dc702fcdc0e
dream-07 <86059280+dream-07@users.noreply.github.com>
2021-09-02 20:20:07 +0530

    Add files via upload

---
+++ js/stone.js
@@ -0,0 +1,25 @@
+class Stone {
+    constructor(x, y) {
+      var options = {
+        restitution: 0.8,
+        friction: 1.0,
+        density: 1.0,
+      };
+      this.r = 30;
+      this.body = Bodies.circle(x, y, this.r, options);
+      World.add(world, this.body);
+    }
+  
+  
+    display() {
+      var pos = this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      noStroke();
+    fill("white");
+      pop();
+  
+    }
+  }
+  
---
+++ sketch.js
@@ -0,0 +1,44 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+var ground,left,right,bridge,jointLink,joint;
+var stones = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  engine = Engine.create();
+  world = engine.world;
+  frameRate(80);
+
+  ground = new Base(width/2,height-20,width,20)
+  left = new Base(200,height/2,400,100)
+  right = new Base(width-200,height/2,400,100)
+bridge = new Bridge(30,{x:400,y:height/2})
+joint = new Base(width-400,height/2,10,20)
+Matter.Composite.add(bridge.body,joint)
+jointLink = new Link(bridge,joint)
+}
+
+function draw() {
+  background(51);
+  Engine.update(engine);
+//rectMode(CENTER)
+
+for (var i = 0; i <= 8; i++) {
+var x = random(width / 2 - 200, width / 2 + 300); 
+var y = random(-10, 140);
+var stone = new Stone (x, y, 80, 80);
+stones.push(stone);
+}
+  stone.display();
+  ground.display();
+  left.display();
+  right.display();
+  bridge.show();
+}
